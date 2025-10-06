92fa6495a91ca205f27d678ff13f9e52e1fb8f1e
Mohamed Ibrahem <65599462+mo7mad1996@users.noreply.github.com>
2025-10-06 15:38:08 +0300

    Updated cybrid.controller.ts

--- src/providers/cybrid/cybrid.controller.ts
+++ src/providers/cybrid/cybrid.controller.ts
@@ -1,5 +1,6 @@
 import { ListBanksRequest } from '@cybrid/cybrid-api-bank-typescript';
 import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
 import { BANK_PROVIDER_SERVICE_KEY } from './cybrid.interface';
 import { CybridService } from './cybrid.service';
 
@@ -11,6 +12,36 @@ export class CybridController {
   ) {}
 
   @Get('banks')
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: String,
+    description: 'Page number',
+  })
+  @ApiQuery({
+    name: 'per_page',
+    required: false,
+    type: String,
+    description: 'Number of items per page',
+  })
+  @ApiQuery({
+    name: 'guid',
+    required: false,
+    type: String,
+    description: 'Bank GUID',
+  })
+  @ApiQuery({
+    name: 'name',
+    required: false,
+    type: String,
+    description: 'Bank name',
+  })
+  @ApiQuery({
+    name: 'type',
+    required: false,
+    type: String,
+    description: 'Bank type',
+  })
   listBanks(@Query() query: ListBanksRequest) {
     return this.cybridService.bankAdaptor.listBanks(query);
   }
